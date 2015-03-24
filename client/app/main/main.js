3f9df2df1b1c98595061a3d253728ea2bd5fe579
Alex Thomas <alexcthomas18@gmail.com>
2015-03-23 18:41:00 -0600

    this is the first commit

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('englishClubApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });

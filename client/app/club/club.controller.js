8dff81e955c7806ab6d58337752a168f9f42efde
Stetson Wood <stetson.wood@vivint.com>
2015-04-13 16:56:58 -0600

    Adding actual club data to club.html

--- client/app/club/club.controller.js
+++ client/app/club/club.controller.js
@@ -1,35 +1,21 @@
 'use strict';
 
 angular.module('englishClubApp')
-  .controller('ClubCtrl', function ($scope,$stateParams) {
-  	$scope.stateParams = $stateParams;
-    $scope.message = 'Hello';
-
-    $scope.persons = [
-    	{
-    		name:"Alex",
-    	},
-    	{
-    		name:"Paul"
-    	}
-    ]
-
-    $scope.listing = {
-        title:"title",
-        subtitle:"subtitle",
-        classes: [
-            {
-                starttime:"sesh1"
-            },
-            {
-                starttime:"sesh1"
-            },
-
-        ]
-      }
-
-      $scope.addSesh = function(){
-      	$scope.listing.classes.push({starttime:'Im New!'})
-      }
+  .controller('ClubCtrl', function ($scope,$stateParams,$http) {
+   
+    $scope.stateParams = $stateParams;    
+    
+    var path = 'api/listings/' + $stateParams.id;
+    
+    $http.get(path).
+    success(function(data, status, headers, config) {
+      $scope.listing = data;
+    }).error(function(data, status, headers, config) {
+      console.log(data);
+    });
+    
+    $scope.addSesh = function(){
+    	$scope.listing.classes.push({starttime:'Im New!'})
+    }
 
   });
--- client/app/club/club.js
+++ client/app/club/club.js
@@ -4,7 +4,7 @@ angular.module('englishClubApp')
   .config(function ($stateProvider) {
     $stateProvider
       .state('club', {
-        url: '/:country/:city/:club?lat&lng',
+        url: '/:country/:city/:club?lat&lng&id',
         templateUrl: 'app/club/club.html',
         controller: 'ClubCtrl'
       });
